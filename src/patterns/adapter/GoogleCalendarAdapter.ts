2456bc793640802cb24cde2cbd3dd1522f46317a
Rafael Freire <rafaelfmg72@gmail.com>
2026-05-14 17:26:48 -0300

    Fix: typo

--- src/patterns/adapter/GoogleCalendarAdapter.ts
+++ src/patterns/adapter/GoogleCalendarAdapter.ts
@@ -5,14 +5,14 @@ import { GoogleCalendarAPI } from "./GoogleCalendarAPI.js";
 export class GoogleCalendarAdapter implements CalendarioService {
 	private readonly api = new GoogleCalendarAPI();
 
-	private readonly events = new Map<string, string>();
+	private readonly eventosGcal = new Map<string, string>();
 
 	async publicarReserva(
 		reserva: Reserva,
 		nomeUsuario: string,
 		nomeSala: string
 	): Promise<void> {
-		const { id: gcalID } = await this.api.createEvent({
+		const { id: gcalId } = await this.api.createEvent({
 			summary: `Reserva — ${nomeSala}`,
       		description: `Reservado por ${nomeUsuario} (id: ${reserva.id})`,
       		start: {
@@ -32,7 +32,7 @@ export class GoogleCalendarAdapter implements CalendarioService {
 		const gcalId = this.eventosGcal.get(reservaId);
 		if(!gcalId) return;
 
-		await this.api.deleteEvent(gcallId);
-		this.eventosGcal.delete(reservaID);
+		await this.api.deleteEvent(gcalId);
+		this.eventosGcal.delete(reservaId);
 	}
 }

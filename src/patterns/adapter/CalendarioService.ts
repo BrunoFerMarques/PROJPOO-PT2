a0607c7c8446c63b9b9d60f132ed13db8dd23f50
Bruno Fernandes Marques <brunofernandesmarques259@gmail.com>
2026-05-16 14:36:29 -0300

    Fix: corrige typo em publicarReserva e sintaxe em removerReserva na interface CalendarioService

--- src/patterns/adapter/CalendarioService.ts
+++ src/patterns/adapter/CalendarioService.ts
@@ -1,11 +1,11 @@
 import type { Reserva } from "../../domain/Reserva.ts";
 
 export interface CalendarioService {
-	publicarRerserva (
+	publicarReserva(
 		reserva: Reserva,
 		nomeUsuario: string,
 		nomeSala: string
 	): Promise<void>;
 
-	removerReserva(reservaId: string) Promise<void>;
+	removerReserva(reservaId: string): Promise<void>;
 }	
